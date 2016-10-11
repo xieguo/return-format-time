acbdfdf2ff8c46685c9117925c7406753454e1be
xieguo <xieguo2007@126.com>
2016-10-11 14:47:56 +0800

    modified test file

--- test/return-format-time.test.js
+++ test/return-format-time.test.js
@@ -2,7 +2,7 @@ var expect = require('chai').expect;
 require('../return-format-time.js')();
 
 describe('Date format test', function () {
-  it('The current fomat date should be 20161009', function () {
-    expect(new Date().format('yyyymmdd')).to.be.equal('20161009');
+  it('The current fomat date should be 20161011', function () {
+    expect(new Date().format('yyyymmdd')).to.be.equal('20161011');
   })
 });
